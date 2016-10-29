a17a2ee7d83fc1eef54bac565c7642cf3b50fd56
Tms <332477694@qq.com>
2016-10-29 13:38:15 +0800

    init

--- app/src/main/java/com/sjtc/tcmall/my/AddressListActivity.java
+++ app/src/main/java/com/sjtc/tcmall/my/AddressListActivity.java
@@ -3,13 +3,19 @@ package com.sjtc.tcmall.my;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.sjtc.tcmall.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressListActivity extends AppCompatActivity implements View.OnClickListener{
     private ListView lvAddressList;
+    private List<String> list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,12 @@ public class AddressListActivity extends AppCompatActivity implements View.OnCli
         lvAddressList= (ListView) findViewById(R.id.lv_AddressList);
         lvAddressList.setEmptyView(findViewById(R.id.t_lvAddressListEmpty));
         View bLeft = findViewById(R.id.t_bLeft);
+        list=new ArrayList<String>();
+        for (int i=1;i<=20;i++){
+            list.add("item "+i);
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.address_list_item, list);
+        lvAddressList.setAdapter(adapter);
         bLeft.setOnClickListener(this);
     }
 
